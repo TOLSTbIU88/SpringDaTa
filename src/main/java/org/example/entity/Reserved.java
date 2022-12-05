f4ef8971c182d21156e4d1b94105975d80c710f6
TOLSTbIU88 <gamer-9600@yandex.ru>
2022-12-05 23:02:32 +0300

    fgd

---
+++ src/main/java/org/example/entity/Reserved.java
@@ -0,0 +1,43 @@
+package org.example.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Reserved {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(nullable = false)
+    private Calendar startDate;
+
+    @Column(nullable = false)
+    @NonNull
+    private Calendar stopDate;
+
+    @Column(nullable = false)
+    @NonNull
+    private String buyName;
+
+    @Column(nullable = false)
+    @NonNull
+    private BigDecimal price;
+
+}
